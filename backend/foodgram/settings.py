297acb7ffcc09003c872cd3258cbf375957ad70d
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-06-08 16:12:38 +0300

    fix: Изменение конфига на порту

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,8 +122,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,14 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularRedocView.as_view(url_name='schema'),
-         name='redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
