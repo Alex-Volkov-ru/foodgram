6d698d64773a00b33e047530c402bb70f0951ceb
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-08-11 20:46:11 +0300

    fix: compose and nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -143,11 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/django_static/'
+STATIC_ROOT = '/app/collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
