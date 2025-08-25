035971690a64557f47237bc3b49637852421c1bf
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-08-25 19:03:21 +0300

    fix: фикс админки

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -4,13 +4,14 @@ from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 from foodgram.constants import BASIC_MIN_VALUE
-
-from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                     ShoppingCart, Tag)
+from .models import (
+    Favorite, Ingredient, Recipe,
+    RecipeIngredient, ShoppingCart, Tag
+)
 
 
 class RecipeIngredientInline(admin.TabularInline):
-    """Встроенная админка для связи рецепт-ингредиент"""
+    """Встроенная админка для связи рецепт-ингредиент."""
     model = RecipeIngredient
     extra = 1
     min_num = BASIC_MIN_VALUE
@@ -20,7 +21,7 @@ class RecipeIngredientInline(admin.TabularInline):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    """Администрирование тегов"""
+    """Администрирование тегов."""
     list_display = ('id', 'name', 'slug')
     search_fields = ('name', 'slug')
     list_editable = ('name', 'slug')
@@ -29,7 +30,7 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    """Администрирование ингредиентов"""
+    """Администрирование ингредиентов."""
     list_display = ('id', 'name', 'measurement_unit')
     search_fields = ('name',)
     list_filter = ('measurement_unit',)
@@ -39,17 +40,14 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    """Администрирование рецептов"""
+    """Администрирование рецептов."""
     list_display = (
         'id', 'name', 'author', 'cooking_time',
         'favorites_count', 'shopping_carts_count',
         'ingredients_count', 'pub_date', 'image_preview'
     )
     list_filter = ('tags', 'author')
-    search_fields = (
-        'name', 'author__username',
-        'author__email', 'tags__name'
-    )
+    search_fields = ('name', 'author__username', 'author__email', 'tags__name')
     readonly_fields = (
         'favorites_count', 'shopping_carts_count', 'pub_date', 'image_preview')
     filter_horizontal = ('tags',)
@@ -58,41 +56,40 @@ class RecipeAdmin(admin.ModelAdmin):
     ordering = ('-pub_date',)
 
     def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        queryset = queryset.annotate(
+        qs = super().get_queryset(request)
+        return qs.annotate(
             _favorites_count=Count('in_favorites', distinct=True),
             _shopping_carts_count=Count('in_shopping_carts', distinct=True),
-            _ingredients_count=Count('ingredients', distinct=True)
+            _ingredients_count=Count('ingredients', distinct=True),
         )
-        return queryset
 
+    @admin.display(ordering='_favorites_count', description=_('В избранном'))
     def favorites_count(self, obj):
         return obj._favorites_count
-    favorites_count.short_description = _('В избранном')
-    favorites_count.admin_order_field = '_favorites_count'
 
+    @admin.display(ordering='_shopping_carts_count',
+                   description=_('В корзинах'))
     def shopping_carts_count(self, obj):
         return obj._shopping_carts_count
-    shopping_carts_count.short_description = _('В корзинах')
-    shopping_carts_count.admin_order_field = '_shopping_carts_count'
 
+    @admin.display(ordering='_ingredients_count',
+                   description=_('Ингредиентов'))
     def ingredients_count(self, obj):
         return obj._ingredients_count
-    ingredients_count.short_description = _('Ингредиентов')
-    ingredients_count.admin_order_field = '_ingredients_count'
 
+    @admin.display(description=_('Превью'))
     def image_preview(self, obj):
         if obj.image:
             return format_html(
-                '<img src="{}" style="max-height: 50px; max-width: 50px;" />',
-                obj.image.url)
-        return "-"
-    image_preview.short_description = _('Превью')
+                '<img src="{}" style="max-height:50px; max-width:50px;" />',
+                obj.image.url
+            )
+        return "—"
 
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
-    """Администрирование избранных рецептов"""
+    """Администрирование избранных рецептов."""
     list_display = ('id', 'user', 'recipe')
     search_fields = ('user__username', 'user__email', 'recipe__name')
     list_filter = ('user', 'recipe')
@@ -102,7 +99,7 @@ class FavoriteAdmin(admin.ModelAdmin):
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    """Администрирование списка покупок"""
+    """Администрирование списка покупок."""
     list_display = ('id', 'user', 'recipe')
     search_fields = ('user__username', 'user__email', 'recipe__name')
     list_filter = ('user', 'recipe')
